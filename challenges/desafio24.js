4a5b8fb4bcc05d5ec543f98b4b7b1bdd39bb7e79
Antonio Malato <malatoneto@gmail.com>
2022-03-12 12:12:25 -0300

    Projeto mongodb dataflights

---
+++ challenges/desafio24.js
@@ -0,0 +1,12 @@
+db.voos.find(
+  {
+    litrosCombustivel: { $lte: 600, $exists: 1 },
+    $nor: [{ "empresa.nome": "GOL" }, { "empresa.nome": "AZUL" }],
+  },
+  {
+    _id: 0,
+    vooId: 1,
+    "empresa.nome": 1,
+    litrosCombustivel: 1,
+  },
+).limit(1);
